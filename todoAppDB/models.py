868fd720ae7b3a947cf1b46b2d016a1ad80d9a71
kareemzak <kareemahmed96@outlook.com>
2021-03-12 21:29:41 +0200

    added connection to sqlite db

--- todoAppDB/models.py
+++ todoAppDB/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    desc = models.TextField(max_length=500)
+    state = models.BooleanField()
+    priority = models.IntegerField()
+
--- todoAppDB/views.py
+++ todoAppDB/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
+from todoAppDB.models import Task
 
+tasks = Task.objects.all()
 
-tasks = ["task1", "task2", "task3"]
 
 # Create your views here.
 def index(request):
     return render(request, "todoAppDB/index.html", {
         "tasks": tasks
     })
+
