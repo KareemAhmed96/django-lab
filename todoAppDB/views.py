d29d90ee9c658611f0ed7610d29175aea0fe320f
kareemzak <kareemahmed96@outlook.com>
2021-03-12 20:41:58 +0200

    created dynamic HTML table structure for the todoAppDB

--- todoAppDB/views.py
+++ todoAppDB/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 
 
+tasks = ["task1", "task2", "task3"]
+
 # Create your views here.
 def index(request):
-    return render(request, "todoAppDB/index.html")
+    return render(request, "todoAppDB/index.html", {
+        "tasks": tasks
+    })
